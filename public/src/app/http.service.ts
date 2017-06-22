5219c63048b4ba82630d0cfffc9030077604e276
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-06-22 12:21:02 -0700

    fixing merge conflicts

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -7,23 +7,12 @@ export class HttpService {
 
   constructor(private _http: Http) { }
 
-<<<<<<< HEAD
-  // function for login
-  login(userName) {
-    console.log("hit http.service login function", userName);
-  }
-
-  // function (for dashboard component) to get all topics (to display on dashboard)
-  getTopics() {
-    return this._http.get('/dashboard').map((data)=>data.json()).toPromise()
-=======
   getNotes(){
     return this._http.get("/notes").map(response=>response.json()).toPromise();
   }
 
   addNote(note){
     return this._http.post("/addnote", note).map(response=>response.json()).toPromise();
->>>>>>> 0e23ecbab378201c79073e1f1d6124e60aea4069
   }
 
   // function to check users
--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('./server/config/mongoose.js');
-=======
- // 
-// REQUIRES
-//
-var express = require('express');
-var path = require('path');
-var app = express();
-var bodyParser = require('body-parser');
->>>>>>> 0e23ecbab378201c79073e1f1d6124e60aea4069
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './public/dist')));
